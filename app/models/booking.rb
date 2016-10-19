d82eef4ec972b958b898262d5eb7e3e371d8e341
Marcos Beirigo <marcosbeirigo@gmail.com>
2016-10-19 10:20:29 +0800

    David's solutuion for the no show problem

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,25 @@
 class Booking < ApplicationRecord
 
+  NO_SHOW_WINDOW_IN_DAY = 4
+
+  belongs_to :lesson
+  has_one :no_show_charge
+
+  validate :status_check, on: :update
+
+  after_update :create_no_show_charge
+
+  def status_check
+    errors.add(:base, "Booking already marked as `no_show`") if
+      self.status_was == 'no_show'
+  end
+
+  def no_show!
+    # return false if self.status.in? [:canceled, :no_show]
+    self.update(status: :no_show)
+  end
+
+  def create_no_show_charge
+    NoShowCharge.find_or_create_by(booking: self)
+  end
 end

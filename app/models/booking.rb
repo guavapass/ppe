160226c870cd7bf6d001d9b0a68d475afbd16ded
Marcos Beirigo <marcosbeirigo@gmail.com>
2016-10-28 16:07:33 +0800

    Nityam Solution

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,20 @@
 class Booking < ApplicationRecord
+  belongs_to :lesson
+  has_many :no_show_charges
+
+  def no_show!
+    raise_already_no_show if self.status == 'no_show'
+    raise_already_cancelled if self.status == 'cancelled'
+    self.status = 'no_show'
+    self.no_show_charges.create if self.save
+  end
+
+  def raise_already_cancelled
+    raise "Cannot mark no show for `cancelled` bookings."
+  end
+
+  def raise_already_no_show
+    raise "Booking already marked as `no_show`."
+  end
 
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,7 +1,11 @@
 class Lesson < ApplicationRecord
+  has_many :bookings
 
   def duration
     duration_in_minutes.minutes
   end
 
+  def finish_date
+    self.starts_at + duration
+  end
 end

d6ea048ace77cdc93b810aa04888e885b0d5d244
Marcos Beirigo <marcosbeirigo@gmail.com>
2016-11-04 11:13:04 +0800

    Carl's take on the now show problem

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,36 @@
 class Booking < ApplicationRecord
+  has_one :no_show_charge
+  belongs_to :lesson
+  has_one :billing_adjustment
 
+  def no_show!
+    raise "Cannot mark no show for `cancelled` bookings." if self.status == "cancelled"
+    raise "Booking already marked as `no_show`." if self.status == "no_show"
+
+
+    billing_adjustment_create
+
+    self.transaction do
+      self.create_no_show_charge if Time.now < no_show_window
+      self.update!(status: "no_show")
+    end
+  end
+
+  private
+
+  def no_show_window
+    lesson.starts_at + lesson.duration + 4.days
+  end
+
+  def billing_adjustment_create
+    if (payment_range.cover?(Time.now) && !payment_range.cover?(lesson.finished_at))
+      create_billing_adjustment
+    end
+  end
+
+  def payment_range
+    start_of_payment = Time.now.beginning_of_month + 4.days
+    end_of_payment = start_of_payment + 1.month
+    payment_range = start_of_payment..end_of_payment
+  end
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,7 +1,9 @@
 class Lesson < ApplicationRecord
-
   def duration
     duration_in_minutes.minutes
   end
 
+  def finished_at
+    starts_at + duration
+  end
 end

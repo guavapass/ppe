463bcd98bb32dc0355ba381ae73de9abf14594fa
Marcos Beirigo <marcosbeirigo@gmail.com>
2016-11-17 16:01:00 +0800

    Calin's implementation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,32 @@
 class Booking < ApplicationRecord
 
+  belongs_to :lesson
+  has_one :no_show_charge
+  has_one :billing_adjustment
+
+  def no_show!
+    validate_no_show
+    Booking.transaction do
+
+      self.status = 'no_show'
+      if self.lesson.starts_at > (Time.now - (4.days + self.lesson.duration))
+        self.create_no_show_charge
+        self.create_billing_adjustment(price: self.no_show_charge.price) if (self.lesson.starts_at < current_billing_date) && Time.now > current_billing_date
+      end
+      self.save
+    end
+    return true
+  end
+
+  private
+
+  def validate_no_show
+    raise Exception.new('Booking already marked as `no_show`.') if self.status == 'no_show'
+    raise Exception.new("Cannot mark no show for `#{self.status}` bookings.") if !['completed', 'pending'].include? self.status
+  end
+
+  def current_billing_date
+    Date.today.beginning_of_month + 4.days
+  end
+
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -3,102 +3,103 @@ require 'rails_helper'
 RSpec.describe Booking, type: :model do
 
   describe "#no_show!" do
-    # subject { booking.no_show! }
+    subject { booking.no_show! }
 
-    # context "pending booking" do
-    #   let(:lesson) { create(:lesson, starts_at: 1.day.ago) }
-    #   let(:booking) { create(:booking, status: "pending", lesson: lesson ) }
+    context "pending booking" do
+      let(:lesson) { create(:lesson, starts_at: 1.day.ago) }
+      let(:booking) { create(:booking, status: "pending", lesson: lesson ) }
 
-    #   it { is_expected.to be_truthy }
+      it { is_expected.to be_truthy }
 
-    #   it "updates the booking status to 'no_show'" do
-    #     subject
-    #     expect(booking.status).to eq("no_show")
-    #   end
+      it "updates the booking status to 'no_show'" do
+        subject
+        expect(booking.status).to eq("no_show")
+      end
 
-    #   it "creates a NoShowCharge" do
-    #     expect {
-    #       subject
-    #     }.to change { NoShowCharge.count }.from(0).to(1)
-    #   end
-    # end
+      it "creates a NoShowCharge" do
+        subject
+        expect(booking.no_show_charge).to be_persisted
+      end
+    end
 
-    # context "cancelled booking" do
-    #   let(:booking) { create(:booking, status: "cancelled") }
+    context "cancelled booking" do
+      let(:booking) { create(:booking, status: "cancelled") }
 
-    #   it "raises an error" do
-    #     expect {
-    #       subject
-    #     }.to raise_error("Cannot mark no show for `cancelled` bookings.")
-    #   end
-    # end
+      it "raises an error" do
+        expect {
+          subject
+        }.to raise_error("Cannot mark no show for `cancelled` bookings.")
+      end
+    end
 
-    # context "no_show booking" do
-    #   let(:booking) { create(:booking, status: "no_show") }
+    context "no_show booking" do
+      let(:booking) { create(:booking, status: "no_show") }
 
-    #   it "raises an error" do
-    #     expect {
-    #       subject
-    #     }.to raise_error("Booking already marked as `no_show`.")
-    #   end
-    # end
+      it "raises an error" do
+        expect {
+          subject
+        }.to raise_error("Booking already marked as `no_show`.")
+      end
+    end
 
-    # context "completed booking" do
-    #   let(:booking) { create(:booking, status: "completed") }
+    context "completed booking" do
+      let(:booking) { create(:booking, status: "completed") }
 
-    #   it { is_expected.to be_truthy }
+      it { is_expected.to be_truthy }
 
-    #   it "updates the booking status to 'no_show'" do
-    #     subject
-    #     expect(booking.status).to eq("no_show")
-    #   end
-    # end
+      it "updates the booking status to 'no_show'" do
+        subject
+        expect(booking.status).to eq("no_show")
+      end
+    end
 
-    # context "outside the standard no show window" do
-    #   let(:lesson) { create(:lesson, starts_at: Time.now - (4.days + 60.minutes) ) }
-    #   let(:booking) { create(:booking, status: "pending", lesson: lesson) }
+    context "outside the standard no show window" do
+      let(:lesson) { create(:lesson, starts_at: Time.now - (4.days + 60.minutes) ) }
+      let(:booking) { create(:booking, status: "completed", lesson: lesson) }
 
-    #   it { is_expected.to be_truthy }
+      it { is_expected.to be_truthy }
 
-    #   it "updates the booking status to 'no_show'" do
-    #     subject
-    #     expect(booking.status).to eq("no_show")
-    #   end
+      it "updates the booking status to 'no_show'" do
+        subject
+        expect(booking.status).to eq("no_show")
+      end
 
-    #   it "does not create a NoShowCharge" do
-    #     expect {
-    #       subject
-    #     }.to_not change { NoShowCharge.count }
-    #   end
-    # end
+      it "does not create a NoShowCharge" do
+        expect {
+          subject
+        }.to_not change { NoShowCharge.count }
+      end
+    end
 
-    # context "lesson happened before the 5th of the current month" do
-    #   before { Timecop.freeze(Date.new(2016,10,6)) }
-    #   after { Timecop.return }
+    context "lesson happened before the 5th of the current month" do
+      before { Timecop.freeze(Date.new(2016,10,6)) }
+      after { Timecop.return }
 
-    #   let(:lesson) { create(:lesson, starts_at: Date.new(2016,9,25).to_time) }
-    #   let(:booking) { create(:booking, status: "completed", lesson: lesson) }
+      let(:lesson) { create(:lesson, starts_at: Date.new(2016,10,4).to_time) }
+      let(:booking) { create(:booking, status: "completed", lesson: lesson) }
 
-    #   it "creates a billing adjustment" do
-    #     expect {
-    #       subject
-    #     }.to change { BillingAdjustment.count }.from(0).to(1)
-    #   end
-    # end
+      it "creates a billing adjustment" do
+        expect {
+          subject
+        }.to change { BillingAdjustment.count }.from(0).to(1)
 
-    # context "lesson happened after the 5th of the current month" do
-    #   before { Timecop.freeze(Date.new(2016,10,20)) }
-    #   after { Timecop.return }
+        expect(booking.status).to eq("no_show")
+      end
+    end
 
-    #   let(:lesson) { create(:lesson, starts_at: Date.new(2016,10,15).to_time) }
-    #   let(:booking) { create(:booking, status: "completed", lesson: lesson) }
+    context "lesson happened after the 5th of the current month" do
+      before { Timecop.freeze(Date.new(2016,10,20)) }
+      after { Timecop.return }
 
-    #   it "does not create a billing adjustment" do
-    #     expect {
-    #       subject
-    #     }.to_not change { BillingAdjustment.count }
-    #   end
-    # end
+      let(:lesson) { create(:lesson, starts_at: Date.new(2016,10,15).to_time) }
+      let(:booking) { create(:booking, status: "completed", lesson: lesson) }
+
+      it "does not create a billing adjustment" do
+        expect {
+          subject
+        }.to_not change { BillingAdjustment.count }
+      end
+    end
 
   end
 end
